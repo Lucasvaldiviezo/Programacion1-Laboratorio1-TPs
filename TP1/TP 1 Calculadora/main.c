1804c30e7af3b8a88d7468782749c5bb572d5f8a
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2018-09-04 23:17:25 -0300

    Ahora si, calculadora final

--- TP1/TP 1 Calculadora/main.c
+++ TP1/TP 1 Calculadora/main.c
@@ -6,17 +6,17 @@ int main()
 {
     float numero1=0;
     float numero2=0;
-    float resultadosuma=0;
-    float resultadoresta=0;
-    float resultadomultiplicacion=0;
-    float resultadodivision=0;
-    float resultadofactoreo1=0;
-    float resultadofactoreo2=0;
+    float resultadoSuma=0;
+    float resultadoResta=0;
+    float resultadoMultiplicacion=0;
+    float resultadoDivision=0;
+    float resultadoFactoreo1=0;
+    float resultadoFactoreo2=0;
     int reint=10;
-    int error=0;
+    //int error=0;
     int opcion;
     int salir=1;
-    int mostrarresultados=0;
+    int mostrarResultados=0;
     do
     {
 
@@ -37,56 +37,41 @@ int main()
                         fflush(stdin);
                         break;
                     case 3:
-                        utn_suma(&resultadosuma,numero1,numero2);
-                        utn_resta(&resultadoresta,numero1,numero2);
-                        utn_multiplicacion(&resultadomultiplicacion,numero1,numero2);
-                        resultadofactoreo1=utn_factoreo(numero1);
-                        resultadofactoreo2=utn_factoreo(numero2);
-                        if(resultadofactoreo1==-1 || resultadofactoreo2==-1 )
-                        {
-                            error=2;
-                        }
-
-                        if(utn_division(&resultadodivision,numero1,numero2)==-1)
-                        {
-                            if(error==2)
-                            {
-                                error=3;
-                            }else
-                            {
-                                error=1;
-                            }
-
-                        }
-                        mostrarresultados=1;
+                        utn_suma(&resultadoSuma,numero1,numero2);
+                        utn_resta(&resultadoResta,numero1,numero2);
+                        utn_multiplicacion(&resultadoMultiplicacion,numero1,numero2);
+                        resultadoFactoreo1=utn_factoreo(numero1);
+                        resultadoFactoreo2=utn_factoreo(numero2);
+                        utn_division(&resultadoDivision,numero1,numero2);
+                        mostrarResultados=1;
 
                         break;
                     case 4:
-                        if (mostrarresultados==1)
+                        if (mostrarResultados==1)
                         {
-                            utn_texto(resultadosuma,"1)El resultado de A+B es: %.2f \n");
-                            utn_texto(resultadoresta,"2)El resultado de A-B es: %.2f \n");
+                            utn_texto(resultadoSuma,"1)El resultado de A+B es: %.2f \n");
+                            utn_texto(resultadoResta,"2)El resultado de A-B es: %.2f \n");
                             if(numero2==0)
                             {
-                                utn_texto(resultadodivision,"3)No se puede dividir por 0\n");
+                                utn_texto(resultadoDivision,"3)No se puede dividir por 0\n");
                             }else
                             {
-                                utn_texto(resultadodivision,"3)El resultado de A/B es: %.2f \n");
+                                utn_texto(resultadoDivision,"3)El resultado de A/B es: %.2f \n");
                             }
-                            utn_texto(resultadomultiplicacion,"4)El resultado de A*B es: %.2f \n");
+                            utn_texto(resultadoMultiplicacion,"4)El resultado de A*B es: %.2f \n");
                             if(numero1 < 0)
                             {
-                                utn_texto(resultadofactoreo1,"5)No se puede factorear el A ya que es negativo");
+                                utn_texto(resultadoFactoreo1,"5)No se puede factorear el A ya que es negativo");
                             }else
                             {
-                                utn_texto(resultadofactoreo1,"5)El factorial de A es: %.0f");
+                                utn_texto(resultadoFactoreo1,"5)El factorial de A es: %.0f");
                             }
                             if(numero2 < 0)
                             {
-                                utn_texto(resultadofactoreo2," y no se puede factorear el B ya que es negativo\n");
+                                utn_texto(resultadoFactoreo2," y no se puede factorear el B ya que es negativo\n");
                             }else
                             {
-                                utn_texto(resultadofactoreo2," y el factorial de B es: %.0f\n");
+                                utn_texto(resultadoFactoreo2," y el factorial de B es: %.0f\n");
                             }
 
 
