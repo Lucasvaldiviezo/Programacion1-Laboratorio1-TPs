8049be051e472b69926888f834c996483cf96302
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2018-10-31 03:24:24 -0300

    Is valid comentados

--- TP3/TP3/Win_64/Employee.c
+++ TP3/TP3/Win_64/Employee.c
@@ -11,11 +11,22 @@ static int isValidNombre(char* nom);
 static int isValidId(char* employeeId);
 static int isValidHorasTrabajadas(char* horasTrabajadas);
 static int isValidSueldo(char* sueldo);
-
+/**
+    employee_new: crea un nuevo espacio en memoria con una variable de tipo Employee
+    @return direccion de memoria creada.
+*/
 Employee* employee_new(void)
 {
     return (Employee*) malloc(sizeof(Employee));
 }
+/**
+    employee_newParametros: crea un nuevo espacio en memoria y guarda datos en un tipo Employee
+    @param idStr: cadena de caracteres con el id a guardar
+    @param nombreStr: cadena de caracteres con el nombre a guardar
+    @param horasTrabajadasStr: cadena de caracteres con las horas trabajadas a guardar
+    @param sueldoStr: cadena de caracteres con el sueldo a guardar
+    @return direccion de memoria creada.
+*/
 Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr,char* sueldoStr)
 {
     Employee* p;
@@ -34,6 +45,11 @@ Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajad
     }
     return p;
 }
+/**
+    employee_delete: borra un espacio en memoria
+    @param this:el espacio en memoria a borrar
+    @return 0 OK, -1 error.
+*/
 int employee_delete(Employee* this)
 {
     int retorno=-1;
@@ -44,7 +60,12 @@ int employee_delete(Employee* this)
     }
     return retorno;
 }
-
+/**
+    employee_setNombre: recibe un nombre para guardarlo
+    @param this: el espacio en memoria donde se guardara
+    @param nombre: la cadena de caracteres con el nombre
+    @return 0 OK, -1 error.
+*/
 int employee_setNombre(Employee* this,char* nombre)
 {
     int retorno=-1;
@@ -56,7 +77,12 @@ int employee_setNombre(Employee* this,char* nombre)
     }
     return retorno;
 }
-
+/**
+    employee_getNombre: recibe un dato tipo Employee donde se buscara un nombre y se devolvera ese nombre
+    @param this: el espacio en memoria de un dato tipo Employee donde se buscara
+    @param nombre: una cadena de caracteres donde se guardara el nombre a buscar
+    @return 0 OK, -1 error.
+*/
 int employee_getNombre(Employee* this,char* nombre)
 {
     int retorno=-1;
@@ -68,7 +94,11 @@ int employee_getNombre(Employee* this,char* nombre)
     }
     return retorno;
 }
-
+/**
+    employee_isValidHorasTrabajadas: recibe una cadena de caracteres de un nombre para validar que solo sean letras
+    @param nombre: cadena de caracteres a validar
+    @return 0 OK, -1 error.
+*/
 static int isValidNombre(char* nombre)
 {
     int i=0;
@@ -91,7 +121,12 @@ static int isValidNombre(char* nombre)
 
     return retorno;
 }
-
+/**
+    employee_setId: recibe un Id para guardarlo
+    @param this: el espacio en memoria donde se guardara
+    @param id:la cadena de caracteres con el Id
+    @return 0 OK, -1 error.
+*/
 int employee_setId(Employee* this,char* id)
 {
     int retorno=-1;
@@ -115,7 +150,12 @@ int employee_setId(Employee* this,char* id)
     }
     return retorno;
 }
-
+/**
+    employee_getId: recibe un dato tipo Employee donde se buscara un id y se devolvera ese id
+    @param this: el espacio en memoria de un dato tipo Employee donde se buscara
+    @param id: el puntero a un int donde se guardara el Id a buscar
+    @return 0 OK, -1 error.
+*/
 int employee_getId(Employee* this,int* id)
 {
     int retorno=-1;
@@ -126,7 +166,11 @@ int employee_getId(Employee* this,int* id)
      }
     return retorno;
 }
-
+/**
+    employee_isValidHorasTrabajadas: recibe una cadena de caracteres de un id para validar que solo sean numeros
+    @param employeeId: cadena de caracteres a validar
+    @return 0 OK, -1 error.
+*/
 static int isValidId(char* employeeId)
 {
     int i=0;
@@ -150,7 +194,12 @@ static int isValidId(char* employeeId)
 
     return retorno;
 }
-
+/**
+    employee_setHorasTrabajadas: recibe una cantidad de horas trabajadas para guardarla
+    @param this: el espacio en memoria donde se guardara
+    @param horasTrabajadas: la cadena de caracteres con las horas trabajadas
+    @return 0 OK, -1 error.
+*/
 int employee_setHorasTrabajadas(Employee* this,char* horasTrabajadas)
 {
     int retorno=-1;
@@ -162,7 +211,12 @@ int employee_setHorasTrabajadas(Employee* this,char* horasTrabajadas)
     }
     return retorno;
 }
-
+/**
+    employee_getHorasTrabajadas: recibe un dato tipo Employee donde se buscara la cantidad de horas trabajadas y se devolvera esa cantidad
+    @param this: el espacio en memoria de un dato tipo Employee donde se buscara
+    @param horasTrabajadas: el puntero a un int donde se guardara las horas a buscar
+    @return 0 OK, -1 error.
+*/
 int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas)
 {
     int retorno=-1;
@@ -174,7 +228,11 @@ int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas)
      }
     return retorno;
 }
-
+/**
+    employee_isValidHorasTrabajadas: recibe una cadena de caracteres de horas trabajadas para validar que solo sean numeros
+    @param horasTrabajadas: cadena de caracteres a validar
+    @return 0 OK, -1 error.
+*/
 static int isValidHorasTrabajadas(char* horasTrabajadas)
 {
     int i=0;
@@ -191,7 +249,12 @@ static int isValidHorasTrabajadas(char* horasTrabajadas)
 
     return retorno;
 }
-
+/**
+    employee_setSueldo: recibe un sueldo para guardarlo
+    @param this: el espacio en memoria donde se guardara
+    @param sueldo: la cadena de caracteres con el sueldo
+    @return 0 OK, -1 error.
+*/
 int employee_setSueldo(Employee* this,char* sueldo)
 {
     int retorno=-1;
@@ -203,7 +266,12 @@ int employee_setSueldo(Employee* this,char* sueldo)
     }
     return retorno;
 }
-
+/**
+    employee_getSueldo: recibe un dato tipo Employee donde se buscara un sueldo y se devolvera ese sueldo
+    @param this: el espacio en memoria de un dato tipo Employee donde se buscara
+    @param sueldo: el puntero a un int donde se guardara el sueldo a buscar
+    @return 0 OK, -1 error.
+*/
 int employee_getSueldo(Employee* this,int* sueldo)
 {
     int retorno=-1;
@@ -215,7 +283,11 @@ int employee_getSueldo(Employee* this,int* sueldo)
      }
     return retorno;
 }
-
+/**
+    employee_isValidSueldo: recibe una cadena de caracteres de sueldo para validar que solo sean numeros
+    @param sueldo: cadena de caracteres a validar
+    @return 0 OK, -1 error.
+*/
 static int isValidSueldo(char* sueldo)
 {
     int i=0;
@@ -232,7 +304,11 @@ static int isValidSueldo(char* sueldo)
 
     return retorno;
 }
-
+/**
+    employee_alta: agrega nuevos datos al array LinkedList.
+    @param arrayLinkedList: recibe el array en el que se cargaran los datos.
+    @return 0 OK, -1 error.
+*/
 int employee_alta(LinkedList* arrayEmployee)
 {
     int retorno=-1;
@@ -260,7 +336,11 @@ int employee_alta(LinkedList* arrayEmployee)
 
     return retorno;
 }
-
+/**
+    employee_modificacion: modifica datos actuales al array LinkedList.
+    @param arrayLinkedList: recibe el array en el que se modificara el dato.
+    @return 0 OK, -1 error.
+*/
 int employee_modificar(LinkedList* arrayEmployee)
 {
     int retorno=-1;
@@ -292,7 +372,11 @@ int employee_modificar(LinkedList* arrayEmployee)
     }
     return retorno;
 }
-
+/**
+    employee_borrar: borra datos actuales al array LinkedList.
+    @param arrayLinkedList: recibe el array en el que se borrara el dato.
+    @return 0 OK, -1 error.
+*/
 int employee_borrar(LinkedList* arrayEmployee)
 {
     int retorno=-1;
@@ -317,41 +401,12 @@ int employee_borrar(LinkedList* arrayEmployee)
     }
     return retorno;
 }
-/*int employee_buscarLugarVacio(Employee* arrayEmployee[], int limite)
-{
-    int retorno=-1;
-    int i;
-    if(arrayEmployee != NULL)
-    {
-        for(i=0;i<limite;i++)
-        {
-            if(arrayEmployee[i] == NULL)
-            {
-                retorno=i;
-                break;
-            }
-        }
-    }
-    return retorno;
-}*/
-
-int employee_inicializarArray(Employee* arrayEmployee[], int limite)
-{
-    int retorno=-1;
-    int i;
-    if(arrayEmployee!=NULL)
-    {
-        retorno=0;
-        for(i=0;i<limite;i++)
-        {
-            arrayEmployee[i]=NULL;
-        }
-    }
-
-
-    return retorno;
-}
-
+/**
+    employee_buscarPorId: recibe una LinkedList y un int con un Id para encontrar en que posicio esta ese Id o si existe
+    @param arrayEmployee: el array a recorrer
+    @param id: el id que se compara con los Id del array hasta encontrar el igual
+    @return un int con la posicion de un array.
+*/
 int employee_buscarPorId(LinkedList* pArrayEmployee, int employeeId)
 {
     int retorno=-1;
@@ -372,7 +427,10 @@ int employee_buscarPorId(LinkedList* pArrayEmployee, int employeeId)
     }
     return retorno;
 }
-
+/**
+    employee_mostrar: lista todo el contenido del array LinkedLista
+    @param arrayLinkedList: recibe el array a mostrar
+*/
 void employee_mostrar(LinkedList* arrayLinkedList)
 {
     int i;
@@ -437,7 +495,12 @@ int employee_ordenarPorNombre(LinkedList* arrayEmployee)
     }
     return retorno;
 }
-
+/**
+    employee_guardarTexto: guarda los datos del LinkedList en la ruta del archivo recibido;
+    @param arrayLinkedList: recibe el array a guardar.
+    @param path: nombre del archivo donde se guardaran los datos
+    @return 0 OK, -1 error.
+*/
 int employee_guardarTexto(LinkedList* pArrayEmployee, char* path)
 {
     int retorno=-1;
--- TP3/TP3/Win_64/Employee.h
+++ TP3/TP3/Win_64/Employee.h
@@ -7,119 +7,29 @@ typedef struct
     int horasTrabajadas;
     int sueldo;
 }Employee;
-/**
-    employee_new: crea un nuevo espacio en memoria con una variable de tipo Employee
-    @return direccion de memoria creada.
-*/
+
 Employee* employee_new();
-/**
-    employee_newParametros: crea un nuevo espacio en memoria y guarda datos en un tipo Employee
-    @param idStr: cadena de caracteres con el id a guardar
-    @param nombreStr: cadena de caracteres con el nombre a guardar
-    @param horasTrabajadasStr: cadena de caracteres con las horas trabajadas a guardar
-    @param sueldoStr: cadena de caracteres con el sueldo a guardar
-    @return direccion de memoria creada.
-*/
 Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr,char* sueldoStr);
-/**
-    employee_delete: borra un espacio en memoria
-    @param this:el espacio en memoria a borrar
-    @return 0 OK, -1 error.
-*/
 int employee_delete(Employee* this);
-/**
-    employee_setId: recibe un Id para guardarlo
-    @param this: el espacio en memoria donde se guardara
-    @param id:la cadena de caracteres con el Id
-    @return 0 OK, -1 error.
-*/
+
 int employee_setId(Employee* this,char* id);
-/**
-    employee_getId: recibe un dato tipo Employee donde se buscara un id y se devolvera ese id
-    @param this: el espacio en memoria de un dato tipo Employee donde se buscara
-    @param id: el puntero a un int donde se guardara el Id a buscar
-    @return 0 OK, -1 error.
-*/
 int employee_getId(Employee* this,int* id);
-/**
-    employee_setNombre: recibe un nombre para guardarlo
-    @param this: el espacio en memoria donde se guardara
-    @param nombre: la cadena de caracteres con el nombre
-    @return 0 OK, -1 error.
-*/
+
 int employee_setNombre(Employee* this,char* nombre);
-/**
-    employee_getNombre: recibe un dato tipo Employee donde se buscara un nombre y se devolvera ese nombre
-    @param this: el espacio en memoria de un dato tipo Employee donde se buscara
-    @param nombre: una cadena de caracteres donde se guardara el nombre a buscar
-    @return 0 OK, -1 error.
-*/
 int employee_getNombre(Employee* this,char* nombre);
-/**
-    employee_setHorasTrabajadas: recibe una cantidad de horas trabajadas para guardarla
-    @param this: el espacio en memoria donde se guardara
-    @param horasTrabajadas: la cadena de caracteres con las horas trabajadas
-    @return 0 OK, -1 error.
-*/
+
 int employee_setHorasTrabajadas(Employee* this,char* horasTrabajadas);
-/**
-    employee_getHorasTrabajadas: recibe un dato tipo Employee donde se buscara la cantidad de horas trabajadas y se devolvera esa cantidad
-    @param this: el espacio en memoria de un dato tipo Employee donde se buscara
-    @param horasTrabajadas: el puntero a un int donde se guardara las horas a buscar
-    @return 0 OK, -1 error.
-*/
 int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas);
-/**
-    employee_setSueldo: recibe un sueldo para guardarlo
-    @param this: el espacio en memoria donde se guardara
-    @param sueldo: la cadena de caracteres con el sueldo
-    @return 0 OK, -1 error.
-*/
+
 int employee_setSueldo(Employee* this,char* sueldo);
-/**
-    employee_getSueldo: recibe un dato tipo Employee donde se buscara un sueldo y se devolvera ese sueldo
-    @param this: el espacio en memoria de un dato tipo Employee donde se buscara
-    @param sueldo: el puntero a un int donde se guardara el sueldo a buscar
-    @return 0 OK, -1 error.
-*/
 int employee_getSueldo(Employee* this,int* sueldo);
-/**
-    employee_mostrar: lista todo el contenido del array LinkedLista
-    @param arrayLinkedList: recibe el array a mostrar
-*/
+
 void employee_mostrar(LinkedList* arrayLinkedList);
-/**
-    employee_alta: agrega nuevos datos al array LinkedList.
-    @param arrayLinkedList: recibe el array en el que se cargaran los datos.
-    @return 0 OK, -1 error.
-*/
 int employee_alta(LinkedList* arrayEmployee);
-/**
-    employee_modificacion: modifica datos actuales al array LinkedList.
-    @param arrayLinkedList: recibe el array en el que se modificara el dato.
-    @return 0 OK, -1 error.
-*/
 int employee_modificar(LinkedList* arrayEmployee);
-/**
-    employee_borrar: borra datos actuales al array LinkedList.
-    @param arrayLinkedList: recibe el array en el que se borrara el dato.
-    @return 0 OK, -1 error.
-*/
 int employee_borrar(LinkedList* arrayEmployee);
-/**
-    employee_buscarPorId: recibe una LinkedList y un int con un Id para encontrar en que posicio esta ese Id o si existe
-    @param arrayEmployee: el array a recorrer
-    @param id: el id que se compara con los Id del array hasta encontrar el igual
-    @return un int con la posicion de un array.
-*/
 int employee_buscarPorId(LinkedList* pArrayEmployee, int employeeId);
 int employee_ordenarPorNombre(LinkedList* arrayEmployee);
-/**
-    employee_guardarTexto: guarda los datos del LinkedList en la ruta del archivo recibido;
-    @param arrayLinkedList: recibe el array a guardar.
-    @param path: nombre del archivo donde se guardaran los datos
-    @return 0 OK, -1 error.
-*/
 int employee_guardarTexto(LinkedList* pArrayEmployee, char* path);
 
 #endif // employee_H_INCLUDED
